db740f5335057cbb61b87dd8404ba43cab8a3a81
Fiorela Maldonado <fiorelamagri@gmail.com>
2022-05-06 17:15:50 -0400

    scroll arreglado

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,15 +10,18 @@ $(document).ready(function(){
             )
         });
     });   
+
 // Cambio de color NavBar
 $(window).scroll(function(){
     $("nav").toggleClass("scrolled",$(this).scrollTop()>50);
 });
+
 // Popovers
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
 // Tooltips
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
