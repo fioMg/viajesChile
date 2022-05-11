69583e6fd49d6eabc9fada9349cfdf07c0b29c6b
Fiorela Maldonado <fiorelamagri@gmail.com>
2022-05-10 21:04:32 -0400

    Correcciones de formulario y jsÂ

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ $(window).scroll(function(){
     $("nav").toggleClass("scrolled",$(this).scrollTop()>50);
 });
 
-// Popovers
+// Popovers Bootstrap v5.1
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
